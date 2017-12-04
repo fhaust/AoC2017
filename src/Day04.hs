34548051a4e46a62fd0acb2370db95a688e79891
Florian Hofmann <fho@f12n.de>
2017-12-04 18:19:34 +0100

    well ... that's easier

--- src/Day04.hs
+++ src/Day04.hs
@@ -2,7 +2,7 @@
 
 module Day04 where
 
-import Data.List (nub, permutations)
+import Data.List (nub, delete, elem, sort)
 
 parseInput = map words . lines
 
@@ -11,6 +11,6 @@ condition1 l = nub l == l
 
 run1 i = length . filter condition1 $ i
 
-condition2 l = not (any (\w -> w `elem` (concatMap permutations . filter (/= w) $ l)) l)
+condition2 l = not (any (\w -> w `elem` delete w l') l') where l' = map sort l
 
 run2 i = length . filter (\l -> condition1 l && condition2 l) $ i
